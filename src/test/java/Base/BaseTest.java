a33d8b1e0502e60990d8f3a164f211a89ffe8de8
Yroslavadudko <dudkomykola@icloud.com>
2024-01-09 11:57:53 +0200

    Sign in

--- src/test/java/Base/BaseTest.java
+++ src/test/java/Base/BaseTest.java
@@ -7,7 +7,7 @@ public class BaseTest {
 
     @BeforeClass
     public static void setUp() {
-        Configuration.baseUrl = "http://localhost:8080";
+        Configuration.baseUrl = "http://127.0.0.1";
         Configuration.browserSize = "1920x1080";
     }
 }
--- src/test/java/Cursova/LoginUiTests.java
+++ src/test/java/Cursova/LoginUiTests.java
@@ -4,27 +4,29 @@ import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
+import Base.BaseTest;
 
-public class LoginUiTests {
+public class LoginUiTests extends BaseTest{
 
     @Test (groups = "Login test")
     public void positiveLoginTest() {
         // A positive test for entry
         open("/"); // Home page
-        $("#j_username").setValue("dudko_78");
-        $("#j_password").setValue("15032005");
+        $("#form-username").setValue("admin");
+        $("#form-password").setValue("admin");
         $(By.xpath("//button[contains(text(),'Sign in')]")).click();
-        $(By.xpath("//span[contains(text(),'Mykola Dudko')]"))
-                .shouldHave(text("Mykola Dudko"));
-        $(By.xpath(" //span[contains(text(),'log out')]")).click();
+        $(By.xpath("//header/div[3]/div[2]/a[1]/i[1]")).click();
+        $(By.xpath("//body/div[@id='dropdown']/ul[1]/li[1]"))
+                .shouldHave(text("admin"));
+        $(By.xpath("//body/div[@id='dropdown']/ul[1]/li[10]/a[1]")).click();
     }
     @Test (groups = "Login test")
     public void negativeLoginTestInvalidUsername() {
         // Negative login test - invalid login
         open("/");
-        $("#j_username").setValue("invalidUsername");
-        $("#j_password").setValue("15032005");
-        $(By.xpath(" //button[contains(text(),'Sign in')]")).click();
+        $("#form-username").setValue("invalidUsername");
+        $("#form-password").setValue("admin");
+        $(By.xpath("/button[contains(text(),'Sign in')]")).click();
         $(By.xpath("//div[contains(text(),'Invalid username or password')]"))
                 .shouldHave(text("Invalid username or password"));
     }
@@ -32,9 +34,9 @@ public class LoginUiTests {
     public void negativeLoginTestInvalidPassword() {
         // A negative login test is an incorrect password
         open("/");
-        $("#j_username").setValue("dudko_78");
-        $("#j_password").setValue("invalidPassword");
-        $(By.xpath(" //button[contains(text(),'Sign in')]")).click();
+        $("#form-username").setValue("admin");
+        $("#form-password").setValue("invalidPassword");
+        $(By.xpath("/button[contains(text(),'Sign in')]")).click();
         $(By.xpath("//div[contains(text(),'Invalid username or password')]"))
                 .shouldHave(text("Invalid username or password"));
     }
