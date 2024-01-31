2aa17b19f68c83a5e3cf64188b569e40434d8faf
Yroslavadudko <dudkomykola@icloud.com>
2024-01-31 22:19:06 +0200

    localhost

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,7 +8,7 @@ pipeline {
     }
     parameters {
          string(name: 'MyRegressionSuite', description: 'Enter the name of the Test Suite', defaultValue: 'MyTestSuite')
-         string(name: 'baseUrl', description: 'Validate Application URL', defaultValue: 'http://localhost/login')
+         string(name: 'baseUrl', description: 'Validate Application URL', defaultValue: 'http://localhost')
     }
     environment {
         mailRecipients = 'dudkomykola@icloud.com'
