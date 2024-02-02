3fdcab89e6bb72f6410e8b303f783f703e17a076
Yroslavadudko <dudkomykola@icloud.com>
2024-02-02 11:20:00 +0200

    myProject fix selectors

--- src/main/java/elements/ProjectElements.java
+++ src/main/java/elements/ProjectElements.java
@@ -6,7 +6,7 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class ProjectElements {
     public static SelenideElement newProjectField(){
-        return $("section.page:nth-child(2) section:nth-child(1) div.page-header ul:nth-child(1) li:nth-child(1) > a.js-modal-medium");
+        return $(By.xpath("//body/section[1]/section[1]/div[1]/ul[1]/li[1]/a[1]"));
     }
     public static SelenideElement formNameProjectField(){
         return $("#form-name");
@@ -27,7 +27,7 @@ public class ProjectElements {
         return $(By.xpath("//body/section[1]/section[1]/div[1]/div[1]/div[1]/a[1]/i[2]"));
     }
     public static SelenideElement myProjects(){
-        return $("section.page:nth-child(2) section.sidebar-container div.sidebar-content div.table-list div.table-list-row.table-border-left div:nth-child(1) > span.table-list-title");
+        return $(By.xpath("//a[contains(text(),'ProjectDudko1')]"));
     }
     public static SelenideElement configureProject(){
         return $(By.xpath("//body/div[@id='dropdown']/ul[1]/li[10]/a[1]"));
